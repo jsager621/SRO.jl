e35986e451f35058edd0188231e3feddf0c062b5
Jens Sager <jens.sager@uni-oldenburg.de>
2024-07-19 11:47:58 +0200

    add stubs for most things

---
+++ src/solvers/copula_correlated/fk_fitting.jl
@@ -0,0 +1,3 @@
+"""
+Collection of solvers relying on full information on correlations given as a copula.
+"""
---
+++ src/solvers/copula_correlated/oracle.jl
@@ -0,0 +1,3 @@
+"""
+Solver assuming information about actual rolled values for problem instances.
+"""
